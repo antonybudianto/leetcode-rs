534778eb2ee1805c19a1e0f15e6bdd272051396e
Antony Budianto <antonybudianto@gmail.com>
2023-01-21 04:37:37 +0700

    Update letter-combinations-of-a-phone-number.rs

--- letter-combinations-of-a-phone-number.rs
+++ letter-combinations-of-a-phone-number.rs
@@ -1,77 +1,61 @@
-use std::collections::HashMap;
 struct Solution {}
+
+const PHONE_MAP: [[char; 4]; 8] = [
+    ['a', 'b', 'c', '-'],
+    ['d', 'e', 'f', '-'],
+    ['g', 'h', 'i', '-'],
+    ['j', 'k', 'l', '-'],
+    ['m', 'n', 'o', '-'],
+    ['p', 'q', 'r', 's'],
+    ['t', 'u', 'v', '-'],
+    ['w', 'x', 'y', 'z'],
+];
+
 impl Solution {
-    pub fn get_chars(map: &HashMap<i8, [char; 4]>, i: i8) -> Option<[char; 4]> {
-        match map.get(&i) {
-            Some(phone) => {
-                return Some(*phone);
-            }
-            None => None,
-        }
+    pub fn index_to_digit(s: &String, index: usize) -> usize {
+        let digit = s.chars().nth(index).unwrap();
+        let digit = digit.to_digit(10).unwrap() as usize;
+        return digit - 2;
     }
 
-    pub fn index_to_digit(s: &String, index: usize) -> i8 {
-        let digit = s.chars().nth(index as usize).unwrap();
-        let digit = digit.to_digit(10).unwrap() as i8;
-        digit
-    }
-
-    pub fn traverse_ch(
-        v: &mut Vec<String>,
-        m: &HashMap<i8, [char; 4]>,
-        sm: &String,
-        s: &String,
-        idx: usize,
-        cidx: usize,
-    ) {
+    pub fn traverse_ch(v: &mut Vec<String>, sm: &String, s: &String, idx: usize, cidx: usize) {
         if idx + 1 > s.len() {
             v.push(sm.to_string());
         } else {
             let digit = Solution::index_to_digit(s, idx);
-            let chrs = Solution::get_chars(m, digit).unwrap();
+            let chrs = PHONE_MAP[digit];
 
             let ch = chrs[cidx];
             let mut sm2 = String::from(sm);
             sm2.push(ch);
 
             if idx + 1 < s.len() {
-                let nx_digit = s.chars().nth(idx + 1).unwrap();
-                let nx_digit = nx_digit.to_digit(10).unwrap() as i8;
-                let nx_chrs = Solution::get_chars(m, nx_digit).unwrap();
+                let nx_digit = Solution::index_to_digit(s, idx + 1);
+                let nx_chrs = PHONE_MAP[nx_digit];
+
                 for i in 1..nx_chrs.len() {
                     if nx_chrs[i] != '-' {
-                        Solution::traverse_ch(v, m, &sm2, s, idx + 1, i);
+                        Solution::traverse_ch(v, &sm2, s, idx + 1, i);
                     }
                 }
             }
 
-            Solution::traverse_ch(v, m, &sm2, s, idx + 1, 0);
+            Solution::traverse_ch(v, &sm2, s, idx + 1, 0);
         }
     }
 
-    pub fn traverse(v: &mut Vec<String>, m: &HashMap<i8, [char; 4]>, s: &String, index: u32) {
-        let digit = Solution::index_to_digit(s, index as usize);
-        let chrs = Solution::get_chars(m, digit).unwrap();
+    pub fn traverse(v: &mut Vec<String>, s: &String, index: usize) {
+        let digit = Solution::index_to_digit(s, index);
+        let chrs = PHONE_MAP[digit];
 
         for i in 0..chrs.len() {
             if chrs[i] != '-' {
-                Solution::traverse_ch(v, &m, &"".to_string(), s, 0, i);
+                Solution::traverse_ch(v, &"".to_string(), s, 0, i);
             }
         }
     }
 
     pub fn letter_combinations(digits: String) -> Vec<String> {
-        let phone_map: HashMap<i8, [char; 4]> = HashMap::from([
-            (2, ['a', 'b', 'c', '-']),
-            (3, ['d', 'e', 'f', '-']),
-            (4, ['g', 'h', 'i', '-']),
-            (5, ['j', 'k', 'l', '-']),
-            (6, ['m', 'n', 'o', '-']),
-            (7, ['p', 'q', 'r', 's']),
-            (8, ['t', 'u', 'v', '-']),
-            (9, ['w', 'x', 'y', 'z']),
-        ]);
-
         let len = digits.len();
         let mut v = Vec::<String>::new();
 
@@ -79,22 +63,19 @@ impl Solution {
             return v;
         } else if len == 1 {
             for chr in digits.chars() {
-                let chr = chr.to_digit(10).unwrap() as i8;
-                match phone_map.get(&chr) {
-                    Some(phone) => {
-                        for ph in 0..phone.len() {
-                            if phone[ph] != '-' {
-                                v.push(phone[ph].to_string());
-                            }
-                        }
+                let chr = chr.to_digit(10).unwrap() as usize;
+                let phone = PHONE_MAP[chr - 2];
+
+                for ph in 0..phone.len() {
+                    if phone[ph] != '-' {
+                        v.push(phone[ph].to_string());
                     }
-                    None => (),
                 }
             }
             return v;
         } else {
             // traverse from first number/char
-            Solution::traverse(&mut v, &phone_map, &digits, 0);
+            Solution::traverse(&mut v, &digits, 0);
             println!("RES:{:?}", v);
             return v;
         }
